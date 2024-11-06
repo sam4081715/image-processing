0cfc68c00fa2bf715d6f035eac252ce987f9ba26
sam4081715 <sam4081715@gmail.com>
2024-11-06 16:01:01 +0800

    code update

--- code/seg.py
+++ code/seg.py
@@ -9,7 +9,15 @@ img  = cv2.imread(img_path)
 h, w, _ = img.shape
 
 #gray value
+gray1 = np.zeros([h, w], dtype=int)
+for i in range(h):
+    for j in range(w):
+        gray1[i][j] = 0.114*img[i][j][0] + 0.587*img[i][j][1] + 0.299*img[i][j][2]
+
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#print(gray)
+#print(gray1)
 
 #binary by average value
 
@@ -17,42 +25,71 @@ count = 0
 for i in range(h):
     for j in range(w):
         count += gray[i][j]
-
 thresh = count/(h*w)
-ret, bin = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)
 
-#mediam filter 3*3(blur)
+maxvalue = 255
+
+bin1 = np.zeros([h, w], dtype=int)
+for i in range(h):
+    for j in range(w):
+        if gray[i][j] > thresh:
+            bin1[i][j] = maxvalue
+        else:
+            bin1[i][j] = 0
+
+ret, bin = cv2.threshold(gray, thresh, maxvalue, cv2.THRESH_BINARY)
+
+#print(bin1)
+#print(bin)
+
+#for i in range(h):
+#    for j in range(w):
+#        if bin1[i][j] != bin[i][j]:
+#            print(bin1[i][j], bin[i][j])
+
+#mediam filter 3*3
 gray = np.pad(gray, ((1, 1), (1, 1)), 'constant', constant_values = (0, 0))
 gray1 = np.zeros([h, w], dtype=int)
 
 for i in range(h):
     for j in range(w):
-        count = 0
+        count = []
         for k in range(3):
             for l in range(3):
-                count += gray[i+k][j+l]
+                count.append(gray[i+k][j+l])
 
-        count /= 9
-        gray1[i][j] = count
+        n = len(count)
+        for o in range(n):
+            for p in range(n-o-1):
+                if count[p] > count[p+1]:
+                    count[p], count[p+1] = count[p+1], count[p]
+
+        median = n//2 + 1 if n%2 > 0 else n//2
+        
+        gray1[i][j] = median
 
 #mediam filter 5*5
 gray = np.pad(gray, ((2, 2), (2, 2)), 'constant', constant_values = (0, 0))
-out = np.zeros([h, w], dtype=int)
+gray1 = np.zeros([h, w], dtype=int)
 
 for i in range(h):
     for j in range(w):
-        count = 0
+        count = []
         for k in range(5):
             for l in range(5):
-                count += gray[i+k][j+l]
+                count.append(gray[i+k][j+l])
 
-        count /= 25
-        out[i][j] = count
+        n = len(count)
+        for o in range(n):
+            for p in range(n-o-1):
+                if count[p] > count[p+1]:
+                    count[p], count[p+1] = count[p+1], count[p]
 
-#GaussianBlur
+        median = n//2 + 1 if n%2 > 0 else n//2
+        
+        gray1[i][j] = median
 
+#blur
 
-
-
-plt.imshow(out, cmap=plt.get_cmap('gray'))
-plt.show()
+#plt.imshow(out, cmap=plt.get_cmap('gray'))
+#plt.show()
